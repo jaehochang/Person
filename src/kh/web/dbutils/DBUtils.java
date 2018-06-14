23bb2071366b28564c76fb2904f5489cd85191ed
손지은 <plmn855000@gmail.com>
2018-06-14 11:37:12 +0900

    PersonDAO,personDTO 생성및 작성완료 및 Lib추가

---
+++ src/kh/web/dbutils/DBUtils.java
@@ -0,0 +1,16 @@
+package kh.web.dbutils;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBUtils {
+	public static Connection getConnection() throws Exception {
+		Context context = new InitialContext();
+		DataSource ds = (DataSource) context.lookup("java:/comp/env/oracle");
+		return ds.getConnection();
+	} 
+}
+
