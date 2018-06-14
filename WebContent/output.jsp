3cbc492950a3cd7f6059d8e9f141ec71b02ce0e1
최인형 <sksksrff@gmail.com>
2018-06-14 12:02:48 +0900

    FrontController / output.jsp 내용 추가

--- WebContent/output.jsp
+++ WebContent/output.jsp
@@ -1,12 +1,31 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>Output</title>
 </head>
 <body>
-
+	<c:choose>
+		<c:when test="${empty result}">
+		표시할 값이 없습니다.
+	</c:when>
+		<c:when test="${not empty result}">
+			<table>
+				<tr>
+					<th>이름</th>
+					<th>이메일</th>
+					<th>연락처</th>
+				</tr>
+				<c:forEach var="pdto" items="${result }">
+					<td>${pdto.name }</td>
+					<td>${pdto.email }</td>
+					<td>${pdto.phone }</td>
+				</c:forEach>
+			</table>
+		</c:when>
+	</c:choose>
 </body>
 </html>
--- src/kh/web/controller/FrontController.java
+++ src/kh/web/controller/FrontController.java
@@ -1,27 +1,57 @@
 package kh.web.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/FrontController")
+import kh.web.dao.PersonDAO;
+import kh.web.dto.PersonDTO;
+
+@WebServlet("*.do")
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    public FrontController() {
-        super();
- 
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String requestURI = request.getRequestURI();
+			String contextPath = request.getContextPath();
 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+			String command = requestURI.substring(contextPath.length());
+
+			PersonDAO pdao = new PersonDAO();
+			boolean isRedirect = true;
+			String dst = null;
+
+			if (command.equals("/input.do")) {
+
+			} else if (command.equals("/output.do")) {
+				List<PersonDTO> result = pdao.printData();
+				request.setAttribute("result", result);
+				isRedirect = false;
+				dst = "output.jsp";
+			}
+
+			if (isRedirect) {
+				response.sendRedirect(dst);
+			} else {
+				RequestDispatcher rd = request.getRequestDispatcher(dst);
+				rd.forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		doGet(request, response);
 	}
